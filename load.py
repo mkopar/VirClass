89c6276482117e7e5fc1c3b3b09b1776665ba263
matejkopar <mk0178@student.uni-lj.si>
2016-05-12 13:33:41 +0200

    csv load file - nedokončan

--- load.py
+++ load.py
@@ -1,11 +1,16 @@
+from collections import defaultdict
+import csv
+import gzip
+from itertools import combinations
 import os
 import pickle
 import random
 import math
 import numpy as np
 import sys
-from load_sequences import run
-from load_sequences import get_rec
+from sklearn import cross_validation
+from load_ncbi import run, load_seqs_from_ncbi
+from load_ncbi import get_rec
 import matplotlib.pyplot as plt
 import pylab as P
 
@@ -25,38 +30,41 @@ def one_hot(x, n):
     return o_h
 
 
-def seq_to_bits(vec):
+def seq_to_bits(vec, unique_nucleotides=None, transmission_dict=None):
     """
-    Get sequence and transform it into number representation.
-    :param vec: sequence
-    :return: number representation
+    Get sequence and transform it into number representation. Given parameters set some rules in representation.
+    :param vec: sequence to transform
+    :param unique_nucleotides: number of unique nucleotides in loaded files
+    :param transmission_dict: transmission dictionary - if None, build it here (every nucleotide represents one bit)
+    :return: number representation of vec
     """
+
+    if unique_nucleotides is None:
+        print "Problems - number of unique nucleotides not present. Exiting now."
+        sys.exit(0)
+
+    if transmission_dict is None:
+        transmission_dict = {}
+        for el in unique_nucleotides:
+            transmission_dict[el] = [1 if x == el else 0 for x in unique_nucleotides]
+
     bits_vector = []
     for i, c in enumerate(vec):
-        if c == "A":
-            bits_vector += [1, 0, 0, 0]
-        elif c == "T":
-            bits_vector += [0, 1, 0, 0]
-        elif c == "C":
-            bits_vector += [0, 0, 1, 0]
-        elif c == "G":
-            bits_vector += [0, 0, 0, 1]
+        if c in transmission_dict.keys():
+            bits_vector += transmission_dict[c]
         else:
-            bits_vector += [1, 1, 1, 1]
+            bits_vector += [1 for _ in transmission_dict.keys()]
+        # if c == "A":
+        #     bits_vector += [1, 0, 0, 0]
+        # elif c == "T":
+        #     bits_vector += [0, 1, 0, 0]
+        # elif c == "C":
+        #     bits_vector += [0, 0, 1, 0]
+        # elif c == "G":
+        #     bits_vector += [0, 0, 0, 1]
+        # else:
+        #     bits_vector += [1, 1, 1, 1]
     return bits_vector
-
-    # for i, c in enumerate(vec):
-    #     if c == "A":
-    #         bits_vector.append(0)
-    #     elif c == "T":
-    #         bits_vector.append(0.33)
-    #     elif c == "C":
-    #         bits_vector.append(0.66)
-    #     elif c == "G":
-    #         bits_vector.append(1)
-    #     else:
-    #         bits_vector.append(0.5)
-    # return bits_vector
 
 
 def load_seqs(ids):
@@ -83,12 +91,85 @@ def histogram(values, name):
     plt.clf()
 
 
+def load_from_file(filename):
+    """
+
+    :param filename:
+    :return:
+    """
+    # odpri datoteko (ki je fajn da je gzip in tab delimited)
+    # v vsaki vrstici imamo : OID\tSEKVENCA (dolzine 100)\tKLASIFIKACIJA (oz taksonomija)
+    # !!!!! taksonomija menda ni nujna - ne spomnim se pa, kaj moram nardit, ce ni podane
+    data = defaultdict(list)
+    tax = {}
+    unique_nucleotides = ''
+
+    try:
+        with gzip.open(filename, 'rb') as file:
+            reader = csv.reader(file, delimiter='\t', quotechar='\'')
+            for oid, seq, classification in reader:
+                oid = int(oid)
+                unique_nucleotides += ''.join(set(seq))
+                unique_nucleotides = ''.join(set(unique_nucleotides))
+                #data.append((oid, seq))
+                data[oid].append(seq)
+                tax[oid] = classification
+    except IOError:
+        data, tax = load_seqs_from_ncbi(seq_len=100, skip_read=0, overlap=50)
+        # sava data
+        with gzip.open(filename, 'wb') as file:
+            csv_file = csv.writer(file, delimiter='\t', quotechar='\'')
+            for oid, seq in data.iteritems():
+                oid = int(oid)
+                # drugace najdi unique nucleotides
+                # sestavi vse sekvence in potem set
+                unique_nucleotides += set([])
+                unique_nucleotides += ''.join(set(seq))
+                unique_nucleotides = ''.join(set(unique_nucleotides))
+                taxonomy_part = tax[oid]
+                # prepare row
+                row = [oid, seq, taxonomy_part]
+                csv_file.writerow(row)
+
+    return data, tax, unique_nucleotides
+
+
+def load_data(filename, test=0.2, transmission_dict=None, seed=random.randint(0, sys.maxint)):
+    data, tax, unique_nucleotides = load_from_file(filename)
+    trX = []
+    trY = []
+    teX = []
+    teY = []
+    tr_ids = []
+    te_ids = []
+
+    oids = [int(x) for x in tax.keys()]
+
+    ss = cross_validation.LabelShuffleSplit(oids, n_iter=1, test_size=test, random_state=seed)
+    for train_index, test_index in ss:
+        tr_ids = list(oids[i] for i in train_index)
+        te_ids = list(oids[i] for i in test_index)
+
+        assert(set(tr_ids).intersection(set(te_ids)) == set())
+
+        for tr_id in tr_ids:
+            # samplamo 20% podatkov iz traina
+            class_size = int(math.ceil(len(data[tr_id]) * 0.2))
+            trX.append(random.sample(data[tr_id], class_size))
+            trY.append(tax[tr_id][0:4] * class_size)
+
+
+
+    # naloadej trX ... in vrni te fajle
+    # mogoce lahko nardim podobno k pri seq_load (da se shrani trX, teX ... podatke)
+
+
 def seq_load(ntrain=50000, ntest=10000, onehot=True, seed=random.randint(0, sys.maxint), thresh=0.1):
     """
     In this method we want to simulate sequencing. We create samples in length of seq_len (in our case 100).
 
     We load data from cached files data-ids.pkl.gz and labels.pkl.gz. When we first run it, those files won't exist
-    so script executes imported method 'run' from file load_sequences.py and generates data (genome ids) and
+    so script executes imported method 'run' from file load_ncbi.py and generates data (genome ids) and
     labels (corresponding class ids). We cache those two variables for future executions of script.
 
     When we have data (genome ids) and labels we load sequences for every genome id.
@@ -132,7 +213,6 @@ def seq_load(ntrain=50000, ntest=10000, onehot=True, seed=random.randint(0, sys.
     # check if thresh parameter is valid
     if thresh < 0.0 or thresh > 1.0:
         raise ValueError("Parameter thresh must be set between 0.0 and 1.0.")
-
 
     seq_len = 100
 
@@ -190,8 +270,8 @@ def seq_load(ntrain=50000, ntest=10000, onehot=True, seed=random.randint(0, sys.
             sum_lengths = sum(len(s) for s in data[first:last])
             print "sum lengths of genomes: %d" % sum_lengths
 
-            examples_in_class.append((last-first, sum_lengths))
-            labels_lengths.append((sum_lengths, last-first))
+            examples_in_class.append((last - first, sum_lengths))
+            labels_lengths.append((sum_lengths, last - first))
 
             threshold = thresh * examples_per_class * seq_len
             if sum_lengths > thresh * (examples_per_class * seq_len):
@@ -218,8 +298,8 @@ def seq_load(ntrain=50000, ntest=10000, onehot=True, seed=random.randint(0, sys.
         while temp_count < (ntrain + ntest):
             for label, first, last in labels_to_process:
 
-                vir_idx     = random.choice(range(first, last))  # randomly choose virus from class
-                sample_idx  = random.choice(range(0, (len(data[vir_idx])) - seq_len - 1))  # randomly sample virus
+                vir_idx = random.choice(range(first, last))  # randomly choose virus from class
+                sample_idx = random.choice(range(0, (len(data[vir_idx])) - seq_len - 1))  # randomly sample virus
 
                 if temp_count < ntrain:
                     trX.append(seq_to_bits(data[vir_idx][sample_idx:sample_idx + seq_len]))
@@ -257,10 +337,18 @@ def seq_load(ntrain=50000, ntest=10000, onehot=True, seed=random.randint(0, sys.
         assert test_examples_per_class == teY.count(labels_to_process[-1][0])
 
         print "saving train and test data for seed %d..." % seed
-        pickle.dump(trX, open(dir + "/tr%d-data-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntrain, seed, int(onehot), thresh * 100), "wb"), -1)
-        pickle.dump(trY, open(dir + "/tr%d-labels-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntrain, seed, int(onehot), thresh * 100), "wb"), -1)
-        pickle.dump(teX, open(dir + "/te%d-data-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntest, seed, int(onehot), thresh * 100), "wb"), -1)
-        pickle.dump(teY, open(dir + "/te%d-labels-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntest, seed, int(onehot), thresh * 100), "wb"), -1)
+        pickle.dump(trX, open(
+            dir + "/tr%d-data-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntrain, seed, int(onehot), thresh * 100), "wb"),
+                    -1)
+        pickle.dump(trY, open(
+            dir + "/tr%d-labels-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntrain, seed, int(onehot), thresh * 100),
+            "wb"), -1)
+        pickle.dump(teX, open(
+            dir + "/te%d-data-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntest, seed, int(onehot), thresh * 100), "wb"),
+                    -1)
+        pickle.dump(teY, open(
+            dir + "/te%d-labels-seed_%d-onehot_%d-threshold_%d.pkl.gz" % (ntest, seed, int(onehot), thresh * 100),
+            "wb"), -1)
         print "saving done"
 
     number_of_classes = len(set(labels))
