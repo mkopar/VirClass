9238ba6561ba4ceb81ab0ab552db2f5dd6eb0b91
matejkopar <mk0178@student.uni-lj.si>
2016-12-19 09:25:29 +0100

    dynamic building of neural network

--- VirClass/build-keras.py
+++ VirClass/build-keras.py
@@ -6,8 +6,8 @@ import random
 import time
 from keras.models import Sequential
 from keras.layers import Dense, MaxPooling1D, Activation, Convolution1D, Flatten, Dropout
-from .load_ncbi import get_gids
-from .load import load_data
+from VirClass.VirClass.load_ncbi import get_gids
+from VirClass.VirClass.load import load_data
 
 
 def load_data_sets_from_file(data_set_filename, debug_mode=False, input_length=100):
@@ -39,7 +39,7 @@ def load_data_sets_from_file(data_set_filename, debug_mode=False, input_length=1
         seed = random.randint(0, 4294967295)
         taxonomy_el_count = -1
     if not data_set_filename:
-        data_set_filename = "%s_%d_%.3f_%d_%d_%d_%d%s" % (hashlib.md5(str(sorted(get_gids()))).hexdigest(), depth,
+        data_set_filename = "%s_%d_%.3f_%d_%d_%d_%d%s" % (hashlib.md5(str(sorted(get_gids())).encode('utf-8')).hexdigest(), depth,
                                                           sample, input_length, one_hot, seed, taxonomy_el_count,
                                                           ".fasta.gz")
 
@@ -59,60 +59,47 @@ def init_keras(train_input_shape, p_drop_conv, p_drop_hidden, number_of_classes,
     Last layer is fully connected layer, which connects all the filters to 500 hidden nodes. These nodes are then
     connected to the output nodes.
     """
-    if convolution_params is not None:
-        convolution1_stride = convolution_params[0]
-        stride1 = convolution_params[1]
-        downscale1 = convolution_params[2]
-        stride2 = convolution_params[3]
-        downscale2 = convolution_params[4]
-        stride3 = convolution_params[5]
-        downscale3 = convolution_params[6]
-    else:
+    if convolution_params is None:
         raise AssertionError("USER ERROR - convolution parameters are empty!")
+        # convolution1_stride = convolution_params[0]
+        # stride1 = convolution_params[1]
+        # downscale1 = convolution_params[2]
+        # stride2 = convolution_params[3]
+        # downscale2 = convolution_params[4]
+        # stride3 = convolution_params[5]
+        # downscale3 = convolution_params[6]
     # block of computation
     # border_mode='valid': apply filter wherever it completely overlaps with the input
     temp_model = Sequential()
-    # layer 1
-    temp_model.add(Convolution1D(stride1, convolution1_stride, border_mode='valid', input_shape=train_input_shape))
-    temp_model.add(Activation("relu"))  # rectified linear unit
-    temp_model.add(MaxPooling1D(pool_length=downscale1, stride=stride1, border_mode='valid'))
-    temp_model.add(Dropout(p=p_drop_conv))
 
-    # l1a = rectify(conv2d(X, w, border_mode='valid', subsample=(1, conv1_stride)))
-    #  stride along one (horizontal) dimension only
-    # l1 = max_pool_2d(l1a, (1, downscale1), st=(1, stride1))
-    #  (1,1)=(vertical, horizontal) downscale, st=(1, step): move to every stride1 column and perform max_pooling there
-    # l1 = dropout(l1, p_drop_conv)
+    for i, (conv_stride, stride, downscale, filters) in enumerate(conv_params):
+        if i == 0:
+            temp_model.add(Convolution1D(filters, conv_stride, border_mode='valid',
+                                         input_shape=train_input_shape, subsample_length=stride))
+        else:
+            temp_model.add(Convolution1D(filters, conv_stride, border_mode='valid', subsample_length=stride))
+        temp_model.add(Activation("relu"))  # rectified linear unit
+        temp_model.add(MaxPooling1D(pool_length=downscale, stride=stride, border_mode='valid'))
+        temp_model.add(Dropout(p=p_drop_conv))
+    # temp_model.add(Convolution1D(stride1, convolution1_stride, border_mode='valid', input_shape=train_input_shape))
+    # temp_model.add(Activation("relu"))  # rectified linear unit
+    # temp_model.add(MaxPooling1D(pool_length=downscale1, stride=stride1, border_mode='valid'))
+    # temp_model.add(Dropout(p=p_drop_conv))
+    #
+    # temp_model.add(Convolution1D(stride2, 1, border_mode='valid'))
+    # temp_model.add(Activation("relu"))  # rectified linear unit
+    # temp_model.add(MaxPooling1D(pool_length=downscale2, stride=stride2, border_mode='valid'))
+    # temp_model.add(Dropout(p=p_drop_conv))
+    #
+    # temp_model.add(Convolution1D(stride3, 1, border_mode='valid'))
+    # temp_model.add(Activation("relu"))  # rectified linear unit
+    # temp_model.add(MaxPooling1D(pool_length=downscale3, stride=stride3, border_mode='valid'))
+    # temp_model.add(Dropout(p=p_drop_conv))
 
-    # repeat block
-    # l2a = rectify(conv2d(l1, w2, subsample=(1, 1))) # stride along horizontal
-    # l2 = max_pool_2d(l2a, (1, downscale2), st=(1, stride2))
-    # l2 = dropout(l2, p_drop_conv)
-
-    temp_model.add(Convolution1D(stride2, 1, border_mode='valid'))
-    temp_model.add(Activation("relu"))  # rectified linear unit
-    temp_model.add(MaxPooling1D(pool_length=downscale2, stride=stride2, border_mode='valid'))
-    temp_model.add(Dropout(p=p_drop_conv))
-
-    # l3a = rectify(conv2d(l2, w3, subsample=(1, 1))) # stride along horizontal
-    # l3b = max_pool_2d(l3a, (1, downscale3), st=(1, stride3))
-    # l3 = T.flatten(l3b, outdim=2) # convert from 4tensor to normal matrix
-    # l3 = dropout(l3, p_drop_conv)
-
-    temp_model.add(Convolution1D(stride3, 1, border_mode='valid'))
-    temp_model.add(Activation("relu"))  # rectified linear unit
-    temp_model.add(MaxPooling1D(pool_length=downscale3, stride=stride3, border_mode='valid'))
-    temp_model.add(Dropout(p=p_drop_conv))
     temp_model.add(Flatten())
-
-    # l4 = rectify(T.dot(l3, w4))
-    # l4 = dropout(l4, p_drop_hidden)
-
-    # model.add() T.dot?
     temp_model.add(Activation("relu"))
     temp_model.add(Dropout(p=p_drop_hidden))
 
-    # pyx = softmax(T.dot(l4, w_o))
     temp_model.add(Dense(number_of_classes))
     temp_model.add(Activation("softmax"))
     return temp_model
@@ -147,14 +134,15 @@ if __name__ == "__main__":
     # trteX = trteX.reshape(-1, 1, input_len)
 
     # params for model and cascade initialization
-    conv1_stride = 4
-    stride_1 = 2
-    downscale_1 = 3
-    stride_2 = 2
-    downscale_2 = 2
-    stride_3 = 2
-    downscale_3 = 1
-    conv_params = (conv1_stride, stride_1, downscale_1, stride_2, downscale_2, stride_3, downscale_3)
+    # conv1_stride = 4
+    # stride_1 = 2
+    # downscale_1 = 3
+    # stride_2 = 2
+    # downscale_2 = 2
+    # stride_3 = 2
+    # downscale_3 = 1
+    # conv params: ((conv1_stride, stride1, downscale1, filters1), (conv2_stride, stride2, downscale2, filters2), ...)
+    conv_params = ((4, 2, 3, 32), (1, 2, 2, 48), (1, 2, 1, 64))
     # make dynamic
     # conv_params = [conv1_stride, (stride_1, downscale_1), (stride_2, downscale_2), (stride_3, downscale_3)]
 
@@ -169,6 +157,10 @@ if __name__ == "__main__":
     # compile train and predict function
     # train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
     # predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
+
+    trX = trX.reshape(1, -1, input_len)
+    teX = teX.reshape(1, -1, input_len)
+    trteX = trteX.reshape(1, -1, input_len)
 
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
